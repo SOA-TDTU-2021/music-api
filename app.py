8b6e6f6578083eee3d453a660241e41cb2ff6382
bxsang <xsang.bui@gmail.com>
2021-04-07 06:00:23 +0700

    Implement APIs (#2)
    * Use namespace rest
    
    * Add some APIs
    
    * Implement login API
    
    * Implement user register
    
    * Add flask_jwt_extended and flask_cors
    
    * Run app at port 5000
    
    * Remove ping api
    
    * Remove subsonic-response fields
    
    * Change status field to success
    
    * change getAlbumList2 to getAlbumList
    
    * Require JWT for accessing APIs
    
    * Do not require JWT to get images
    
    * Implement getPlaylists
    
    * Fix classes name
    
    * Implement getAlbumList
    
    * Implement getArtists
    
    * Change Played.play_time to last_play_time
    
    * Implement getAlbum
    
    * Add mock stream api
    
    * Remove wget-log
    
    * Implement star and unstar albums
    
    * Implement getArtist
    
    * Implement get songs by genre
    
    * Implement get songs by genre
    
    * Add mock download api
    
    * Implement get starred tracks
    
    * Implement get random songs
    
    * Implement get playlist api

--- app.py
+++ app.py
@@ -1,10 +1,313 @@
-from config import app, api
-from flask_restplus import Resource
+from config import app, api, jwt, ns_rest
+import db
+from flask import request, send_file, safe_join, jsonify
+from flask_restplus import Resource, fields
+from flask_jwt_extended import current_user, jwt_required, set_access_cookies, unset_jwt_cookies
+from sqlalchemy import asc, desc
+from sqlalchemy.sql.expression import func
 
-@api.route('/hello')
-class HelloWorld(Resource):
+@jwt.user_identity_loader
+def student_identity_lookup(user):
+    return user.email
+
+@jwt.user_lookup_loader
+def student_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return db.get_user_by_email(identity)
+
+@ns_rest.route('/login')
+class UserLogin(Resource):
+    @api.expect(api.model('Login model',
+        {'email': fields.String(required = True),
+            'password': fields.String(required = True)}))
+    def post(self):
+        data = request.get_json()
+        user = db.get_user_by_email(data['email'])
+        if user.check_password(data['password']):
+            return {'success': True, 'message': 'Đăng nhập thành công', 'access_token':  user.generate_jwt()}
+        else:
+            api.abort(403, 'Sai tên đang nhập hoặc mật khẩu', success=False)
+
+@ns_rest.route('/register')
+class UserRegister(Resource):
+    @api.expect(api.model('Register model',
+        {'name': fields.String(required = True),
+            'email': fields.String(required = True),
+            'password': fields.String(required = True)}))
+    def post(self):
+        data = request.get_json()
+        is_success = db.add_user(name=data['name'], email=data['email'], password=data['password']) is not None
+        return {'success': is_success}
+
+@ns_rest.route('/getPlaylists')
+class GetPlaylists(Resource):
+    @jwt_required()
     def get(self):
-        return {'hello': 'world'}
+        try:
+            playlists = db.get_all_playlists()
+            return {'success': True, 'playlists': {'playlist': playlists}}
+        except Exception as e:
+            print(e)
+            return {'success': False, 'message': 'Failed to get playlists'}
+
+@ns_rest.route('/getPlaylist')
+class GetPlaylist(Resource):
+    @jwt_required()
+    def get(self):
+        playlist_id = request.args.get('id')
+        return {'success': True, 'playlist': db.get_playlist(playlist_id)}
+
+@ns_rest.route('/getAlbumList')
+class getAlbumList(Resource):
+    @jwt_required()
+    def get(self):
+        request_type = request.args.get('type')
+        size = request.args.get('size')
+        offset = request.args.get('offset')
+        limit = offset + size
+        try:
+            if request_type == 'random':
+                albums = db.Album.query.order_by(func.random()).offset(offset).limit(limit).all()
+            elif request_type == 'recent':
+                albums = []
+                played = db.Played.query.filter_by(user_id=current_user.id).order_by(desc(db.Played.last_play_time)).offset(offset).limit(limit).all()
+                for p in played:
+                    if p.track.album in albums:
+                        continue
+                    albums.append(p.track.album)
+            elif request_type == 'frequent':
+                albums = []
+                played = db.Played.query.filter_by(user_id=current_user.id).order_by(db.Played.count).offset(offset).limit(limit).all()
+                for p in played:
+                    if p.track.album in albums:
+                        continue
+                    albums.append(p.track.album)
+            elif request_type == 'newest':
+                albums = db.Album.query.order_by(db.Album.title).offset(offset).limit(limit).all()
+            elif request_type == 'alphabeticalByName':
+                albums = db.Album.query.order_by(func.random()).offset(offset).limit(limit).all()
+            elif request_type == 'byGenre':
+                albums = db.Album.query.join(db.Genre).filter_by(name=request.args.get('genre')).offset(offset).limit(limit).all()
+            else:
+                return {'success': False, 'message': 'Wrong album type'}
+            result = []
+            for a in albums:
+                result.append({
+                    'id': a.id,
+                    'name': a.title,
+                    'artist': 'Rosé',
+                    'artistId': '1',
+                    'coverArt': a.cover_image,
+                    'songCount': 6,
+                    'duration': 1678,
+                    'playCount': 1234,
+                    'created': str(a.date_added),
+                    'year': 2007,
+                    'genre': a.genre.name
+                })
+            return {'success': True, 'albumList2': {'album': result}}
+        except Exception as e:
+            print(e)
+            return {'success': False, 'message': 'Failed to get playlists'}
+
+@ns_rest.route('/getAlbum')
+class getAlbumList(Resource):
+    @jwt_required()
+    def get(self):
+        album_id = request.args.get('id')
+        return {'success': True, 'album': db.get_album(current_user, album_id)}
+
+@ns_rest.route('/getCoverArt')
+@api.representation('image/jpeg')
+class getCoverArt(Resource):
+    def get(self):
+        filepath = safe_join("./", "rosé.jpg")
+        return send_file(
+            filename_or_fp=filepath,
+            mimetype="image/jpeg"
+        )
+
+@ns_rest.route('/stream')
+@api.representation('audio/mpeg')
+class stream(Resource):
+    def get(self):
+        track_id = request.args.get('id')
+        filepath = safe_join("./", "test.mp3")
+        return send_file(
+            filename_or_fp=filepath,
+            mimetype="audio/mpeg"
+        )
+
+@ns_rest.route('/download')
+@api.representation('application/x-download')
+class stream(Resource):
+    def get(self):
+        track_id = request.args.get('id')
+        filepath = safe_join("./", "test.mp3")
+        return send_file(
+            filename_or_fp=filepath,
+            mimetype="application/x-download"
+        )
+
+@ns_rest.route('/getArtists')
+class getArtists(Resource):
+    @jwt_required()
+    def get(self):
+        try:
+            artists = db.get_all_artists()
+            return {
+                'success': True,
+                'artists': {
+                    'ignoredArticles': 'The El La Los Las Le',
+                    'index': artists
+                }
+            }
+        except Exception as e:
+            print(e)
+            return {'success': False, 'message': 'Failed to get artists'}
+
+@ns_rest.route('/getArtist')
+class getArtist(Resource):
+    @jwt_required()
+    def get(self):
+        artist_id = request.args.get('id')
+        return {'success': True, 'artist': db.get_artist(artist_id)}
+
+@ns_rest.route('/star')
+class star(Resource):
+    @jwt_required()
+    def get(self):
+        if request.args.get('id'):
+            track_id = request.args.get('id')
+            success = db.star_track(current_user, track_id) is not None
+            return {'success': success}
+        elif request.args.get('albumId'):
+            album_id = request.args.get('albumId')
+            success = db.star_album(current_user, album_id) is not None
+            return {'success': success}
+        else:
+            return {'success': False, 'message': 'Wrong starred type'}
+
+@ns_rest.route('/unstar')
+class unstar(Resource):
+    @jwt_required()
+    def get(self):
+        if request.args.get('id'):
+            track_id = request.args.get('id')
+            success = db.unstar_track(current_user, track_id)
+            return {'success': success}
+        elif request.args.get('albumId'):
+            album_id = request.args.get('albumId')
+            success = db.unstar_album(current_user, album_id)
+            return {'success': success}
+        else:
+            return {'success': False, 'message': 'Wrong unstarred type'}
+
+@ns_rest.route('/getGenres')
+class getGenres(Resource):
+    @jwt_required()
+    def get(self):
+        return {'success': True, 'genres': {'genre': db.get_genres()}}
+
+@ns_rest.route('/getSongsByGenre')
+class getSongsByGenre(Resource):
+    @jwt_required()
+    def get(self):
+        offset = request.args.get('offset')
+        count = request.args.get('count')
+        result = []
+        tracks = db.Track.query.filter_by(genre_name=request.args.get('genre')).offset(offset).limit(count).all()
+        for t in tracks:
+            result.append({
+                'id': t.id,
+                'parent': 25,
+                'isDir': False,
+                'title': t.name,
+                'album': t.album.title,
+                'artist': 'Artist 1',
+                'track': len(t.album.tracks),
+                'genre': t.genre_name,
+                'coverArt': t.album.cover_image,
+                'size': 123456,
+                'contentType': 'audio/mpeg',
+                'suffix': 'mp3',
+                'duration': 285,
+                'bitRate': 128,
+                'path': 'tracks/track1.mp3',
+                'playCount': 1000,
+                'created': str(t.date_added),
+                'starred': str(t.date_added),
+                'albumId': t.album.id,
+                'artistId': '1',
+                'type': 'music'
+            })
+        return {'success': True, 'songsByGenre': {'song': result}}
+
+@ns_rest.route('/getRandomSongs')
+class getRandomSongs(Resource):
+    @jwt_required()
+    def get(self):
+        size = request.args.get('size')
+        result = []
+        tracks = db.Track.query.order_by(func.random()).limit(size).all()
+        for t in tracks:
+            result.append({
+                'id': t.id,
+                'parent': 25,
+                'isDir': False,
+                'title': t.name,
+                'album': t.album.title,
+                'artist': 'Artist 1',
+                'track': len(t.album.tracks),
+                'genre': t.genre_name,
+                'coverArt': t.album.cover_image,
+                'size': 123456,
+                'contentType': 'audio/mpeg',
+                'suffix': 'mp3',
+                'duration': 285,
+                'bitRate': 128,
+                'path': 'tracks/track1.mp3',
+                'playCount': 1000,
+                'created': str(t.date_added),
+                'starred': str(t.date_added),
+                'albumId': t.album.id,
+                'artistId': '1',
+                'type': 'music'
+            })
+        return {'success': True, 'randomSongs': {'song': result}}
+
+@ns_rest.route('/getStarred2')
+class getStarred2(Resource):
+    @jwt_required()
+    def get(self):
+        result = []
+        rating = db.TrackRating.query.filter_by(user_id=current_user.id).all()
+        for r in rating:
+            t = r.track
+            result.append({
+                'id': t.id,
+                'parent': 25,
+                'isDir': False,
+                'title': t.name,
+                'album': t.album.title,
+                'artist': 'Artist 1',
+                'track': len(t.album.tracks),
+                'genre': t.genre_name,
+                'coverArt': t.album.cover_image,
+                'size': 123456,
+                'contentType': 'audio/mpeg',
+                'suffix': 'mp3',
+                'duration': 285,
+                'bitRate': 128,
+                'path': 'tracks/track1.mp3',
+                'playCount': 1000,
+                'created': str(t.date_added),
+                'starred': str(t.date_added),
+                'albumId': t.album.id,
+                'artistId': '1',
+                'type': 'music'
+            })
+        return {'success': True, 'starred2': {'song': result}}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 from config import sql
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 import math, random, uuid
 
@@ -19,15 +21,29 @@ class User(sql.Model):
     rated_albums = sql.relationship('AlbumRating', backref=sql.backref('rated_by', lazy=True))
     rated_tracks = sql.relationship('TrackRating', backref=sql.backref('rated_by', lazy=True))
     rated_playlists = sql.relationship('PlaylistRating', backref=sql.backref('rated_by', lazy=True))
+    played = sql.relationship('Played', backref=sql.backref('user', lazy=True))
 
-albums_artists = sql.Table('albums_artists',
-    sql.Column('album_id', sql.Integer, sql.ForeignKey("album.id"), primary_key=True),
-    sql.Column('artist_id', sql.Integer, sql.ForeignKey("artist.id"), primary_key=True)
-)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def generate_jwt(self):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        return create_access_token(identity=self)
 
 playlists_tracks = sql.Table('playlists_tracks',
     sql.Column('playlist_id', sql.Integer, sql.ForeignKey("playlist.id"), primary_key=True),
     sql.Column('track_id', sql.Integer, sql.ForeignKey("track.id"), primary_key=True)
+)
+
+albums_artists = sql.Table('albums_artists',
+    sql.Column('album_id', sql.Integer, sql.ForeignKey("album.id"), primary_key=True),
+    sql.Column('artist_id', sql.Integer, sql.ForeignKey("artist.id"), primary_key=True)
 )
 
 tracks_artists = sql.Table('tracks_artists',
@@ -44,24 +60,25 @@ class Artist(sql.Model):
     date_added = sql.Column(sql.DateTime, default=datetime.now)
     date_modified = sql.Column(sql.DateTime, default=datetime.now)
     added_by_user_id = sql.Column(sql.Integer, sql.ForeignKey("user.id"))
-    albums = sql.relationship('Album', secondary=albums_artists, lazy='subquery',
-        backref=sql.backref('artists', lazy=True))
-    tracks = sql.relationship('Track', secondary=tracks_artists, lazy='subquery',
-        backref=sql.backref('artists', lazy=True))
 
 class Genre(sql.Model):
-    id = sql.Column(sql.Integer, primary_key=True)
-    name = sql.Column(sql.String(100))
+    name = sql.Column(sql.String(100), primary_key=True)
+    tracks = sql.relationship('Track', backref=sql.backref('genre', lazy=True))
     albums = sql.relationship('Album', backref=sql.backref('genre', lazy=True))
 
 class Track(sql.Model):
     id = sql.Column(sql.Integer, primary_key=True)
     name = sql.Column(sql.String(100))
-    duration = sql.Column(sql.Float)
+    duration = sql.Column(sql.Integer)
     date_added = sql.Column(sql.DateTime, default=datetime.now)
     date_modified = sql.Column(sql.DateTime, default=datetime.now)
+    genre_name = sql.Column(sql.String(100), sql.ForeignKey("genre.name"))
+    album_id = sql.Column(sql.Integer, sql.ForeignKey("album.id"))
     added_by_user_id = sql.Column(sql.Integer, sql.ForeignKey("user.id"))
+    artists = sql.relationship('Artist', secondary=tracks_artists, lazy='subquery',
+        backref=sql.backref('tracks', lazy=True))
     rated = sql.relationship('TrackRating', backref=sql.backref('track', lazy=True))
+    played = sql.relationship('Played', backref=sql.backref('track', lazy=True))
 
 class Album(sql.Model):
     id = sql.Column(sql.Integer, primary_key=True)
@@ -70,8 +87,11 @@ class Album(sql.Model):
     date_release = sql.Column(sql.DateTime, default=datetime(1970, 1, 1))
     date_added = sql.Column(sql.DateTime, default=datetime.now)
     date_modified = sql.Column(sql.DateTime, default=datetime.now)
-    genres_id = sql.Column(sql.Integer, sql.ForeignKey("genre.id"))
+    genre_name = sql.Column(sql.String(100), sql.ForeignKey("genre.name"))
     added_by_user_id = sql.Column(sql.Integer, sql.ForeignKey("user.id"))
+    tracks = sql.relationship('Track', backref=sql.backref('album', lazy=True))
+    artists = sql.relationship('Artist', secondary=albums_artists, lazy='subquery',
+        backref=sql.backref('albums', lazy=True))
     rated = sql.relationship('AlbumRating', backref=sql.backref('album', lazy=True))
 
 class Playlist(sql.Model):
@@ -89,22 +109,258 @@ class Playlist(sql.Model):
 class AlbumRating(sql.Model):
     album_id = sql.Column(sql.Integer, sql.ForeignKey("album.id"), primary_key=True)
     user_id = sql.Column(sql.Integer, sql.ForeignKey("user.id"), primary_key=True)
-    star = sql.Column(sql.Integer)
     time = sql.Column(sql.DateTime, default=datetime.now)
 
 class TrackRating(sql.Model):
     track_id = sql.Column(sql.Integer, sql.ForeignKey("track.id"), primary_key=True)
     user_id = sql.Column(sql.Integer, sql.ForeignKey("user.id"), primary_key=True)
-    star = sql.Column(sql.Integer)
     time = sql.Column(sql.DateTime, default=datetime.now)
 
 class PlaylistRating(sql.Model):
     playlist_id = sql.Column(sql.Integer, sql.ForeignKey("playlist.id"), primary_key=True)
     user_id = sql.Column(sql.Integer, sql.ForeignKey("user.id"), primary_key=True)
-    star = sql.Column(sql.Integer)
     time = sql.Column(sql.DateTime, default=datetime.now)
 
-class PlayCount(sql.Model):
-    track_id = sql.Column(sql.Integer, sql.ForeignKey("track.id"), primary_key=True)
-    user_id = sql.Column(sql.Integer, sql.ForeignKey("user.id"), primary_key=True)
+class Played(sql.Model):
+    id = sql.Column(sql.Integer, primary_key=True)
+    user_id = sql.Column(sql.Integer, sql.ForeignKey("user.id"))
+    track_id = sql.Column(sql.Integer, sql.ForeignKey("track.id"))
     count = sql.Column(sql.Integer)
+    last_play_time = sql.Column('time', sql.DateTime, default=datetime.now)
+
+def get_user_by_id(user_id):
+    return User.query.filter_by(id=user_id).one_or_none()
+
+def get_user_by_email(user_email):
+    return User.query.filter_by(email=user_email).one_or_none()
+
+def add_user(name, email, password):
+    user = User(name=name, email=email)
+    user.set_password(password)
+    sql.session.add(user)
+    try:
+        sql.session.commit()
+        return user.id
+    except Exception as e:
+        return None
+
+def get_all_playlists():
+    result = []
+    playlists = Playlist.query.all()
+    for p in playlists:
+        result.append({
+            'id': p.id,
+            'name': p.name,
+            'comment': '',
+            'owner': p.created_by.name,
+            'public': p.is_public,
+            'songCount': 12,
+            'duration': 2483,
+            'created': str(p.date_added),
+            'changed': str(p.date_modified),
+            'coverArt': p.image
+        })
+    return result
+
+def get_all_artists():
+    artists = Artist.query.all()
+    result = []
+    indexes = {}
+    for a in artists:
+        first_char = a.name[0].upper()
+        count = 0
+        for album in a.albums:
+            count += 1
+        if first_char not in indexes.keys():
+            indexes[first_char] = [{
+                'id': a.id,
+                'name': a.name,
+                'coverArt': a.avatar_file,
+                'artistImageUrl': 'https://lastfm-img2.akamaized.net/i/u/da0f3b84bd72470492de2a2dc58afbe2.png',
+                'albumCount': count
+            }]
+        else:
+            indexes[first_char].append({
+                'id': a.id,
+                'name': a.name,
+                'coverArt': a.avatar_file,
+                'artistImageUrl': 'https://lastfm-img2.akamaized.net/i/u/da0f3b84bd72470492de2a2dc58afbe2.png',
+                'albumCount': count
+            })
+    for i in indexes.keys():
+        result.append({
+            'name': i,
+            'artist': indexes[i]
+        })
+    return result
+
+def get_artist(artist_id):
+    artist = Artist.query.filter_by(id=artist_id).one_or_none()
+    albums = []
+    count = 0
+    for album in artist.albums:
+        count += 1
+        albums.append({
+            'id': album.id,
+            'name': album.title,
+            'artist': 'Artist 1',
+            'artistId': '1',
+            'coverArt': album.cover_image,
+            'songCount': 2,
+            'duration': 1234,
+            'playCount': 100000,
+            'created': str(album.date_added),
+            'year': 2021,
+            'genre': album.genre.name
+        })
+    return {
+        'id': artist.id,
+        'name': artist.name,
+        'description': 'Xin chào, mình là Sang đây!',
+        'coverArt': artist.avatar_file,
+        'artistImageUrl': artist.avatar_file,
+        'albumCount': count,
+        'album': albums
+    }
+
+def get_playlist(playlist_id):
+    playlist = Playlist.query.filter_by(id=playlist_id).one_or_none()
+    tracks = []
+    count = 0
+    for track in playlist.tracks:
+        count += 1
+        tracks.append({
+            'id': track.id,
+            'parent': 25,
+            'isDir': False,
+            'title': track.name,
+            'album': track.album.title,
+            'artist': 'Artist 1',
+            'track': len(track.album.tracks),
+            'genre': track.genre_name,
+            'coverArt': track.album.cover_image,
+            'size': 123456,
+            'contentType': 'audio/mpeg',
+            'suffix': 'mp3',
+            'duration': 285,
+            'bitRate': 128,
+            'path': 'tracks/track1.mp3',
+            'playCount': 1000,
+            'created': str(track.date_added),
+            'starred': str(track.date_added),
+            'albumId': track.album.id,
+            'artistId': '1',
+            'type': 'music'
+        })
+    return {
+        'id': playlist.id,
+        'name': playlist.name,
+        'comment': '',
+        'owner': playlist.created_by.name,
+        'public': playlist.is_public,
+        'songCount': count,
+        'duration': 1234,
+        'created': str(playlist.date_added),
+        'changed': str(playlist.date_modified),
+        'coverArt': playlist.image,
+        'entry': tracks
+    }
+
+def get_album(user, album_id):
+    album = Album.query.filter_by(id=album_id).one_or_none()
+    songs = []
+    starred_album = AlbumRating.query.filter_by(album_id=album.id).filter_by(user_id=user.id).one_or_none()
+    for tr in album.tracks:
+        starred_track = TrackRating.query.filter_by(track_id=tr.id)
+        songs.append({
+            'id': tr.id,
+            'parent': 261,
+            'isDir': False,
+            'title': tr.name,
+            'album': album.title,
+            'artist': 'Artist 1',
+            'tracks': 1,
+            'year': 2021,
+            'genre': album.genre.name,
+            'coverArt': album.cover_image,
+            'size': 100000,
+            'contentType': 'audio/mpeg',
+            'suffix': 'mp3',
+            'duration': 285,
+            'bitRate': 128,
+            'path': 'tracks/track1.mp3',
+            'playCount': 1000,
+            'created': str(tr.date_added),
+            'starred': starred_track is not None,
+            'albumId': album.id,
+            'artistId': 1,
+            'type': 'music'
+        })
+    return {
+        'id': album.id,
+        'name': album.title,
+        'artist': 'Artist 1',
+        'artistId': '1',
+        'coverArt': album.cover_image,
+        'songCount': 2,
+        'duration': 1234,
+        'playCount': 100000,
+        'created': str(album.date_added),
+        'starred': starred_album is not None,
+        'year': 2021,
+        'genre': album.genre.name,
+        'song': songs
+    }
+
+def star_album(user, album_id):
+    star = AlbumRating(album_id=album_id)
+    try:
+        user.rated_albums.append(star)
+        sql.session.add(user)
+        sql.session.commit()
+        return star.album_id
+    except Exception as e:
+        print(e)
+        return None
+
+def unstar_album(user, album_id):
+    try:
+        star = AlbumRating.query.filter_by(album_id=album_id).filter_by(user_id=user.id).one_or_none()
+        sql.session.delete(star)
+        sql.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def star_track(user, track_id):
+    star = TrackRating(track_id=track_id)
+    try:
+        user.rated_tracks.append(star)
+        sql.session.add(user)
+        sql.session.commit()
+        return star.track_id
+    except Exception as e:
+        print(e)
+        return None
+
+def unstar_track(user, track_id):
+    try:
+        star = TrackRating.query.filter_by(track_id=track_id).filter_by(user_id=user.id).one_or_none()
+        sql.session.delete(star)
+        sql.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def get_genres():
+    result = []
+    genres = Genre.query.all()
+    for g in genres:
+        result.append({
+            'value': g.name,
+            'songCount': len(g.tracks),
+            'albumCount': len(g.albums)
+        })
+    return result
